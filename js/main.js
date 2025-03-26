f1ba6a0345c3ae9d5b06a816c11c7a6b93a45cdf
Ahmed <40410642+abutmim@users.noreply.github.com>
2025-03-26 03:45:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function generateImage(designNumber) {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
@@ -6,14 +5,18 @@ function generateImage(designNumber) {
   const selectedFont = document.getElementById('fontSelect').value;
   const img = new Image();
   img.src = `images/design${designNumber}.jpg`;
+
   img.onload = function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    ctx.font = `bold 36px ${selectedFont}`;
-    ctx.fillStyle = '#006699';
-    ctx.textAlign = 'center';
-    ctx.fillText(name, canvas.width / 2, 500);
-    document.getElementById('downloadBtn').href = canvas.toDataURL();
-    document.getElementById('downloadBtn').style.display = 'inline-block';
+    // تحميل الخط أولاً قبل الرسم
+    document.fonts.load(`36px ${selectedFont}`).then(() => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      ctx.font = `bold 36px ${selectedFont}`;
+      ctx.fillStyle = '#006699';
+      ctx.textAlign = 'center';
+      ctx.fillText(name, canvas.width / 2, 500);
+      document.getElementById('downloadBtn').href = canvas.toDataURL();
+      document.getElementById('downloadBtn').style.display = 'inline-block';
+    });
   };
 }
