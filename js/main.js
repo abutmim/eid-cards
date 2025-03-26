134ab39b8abd409fe91fdfa619d39ac5bad4e528
Ahmed <40410642+abutmim@users.noreply.github.com>
2025-03-26 03:33:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ function generateImage(designNumber) {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   const name = document.getElementById('nameInput').value;
+  const selectedFont = document.getElementById('fontSelect').value;
   const img = new Image();
   img.src = `images/design${designNumber}.jpg`;
   img.onload = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    ctx.font = 'bold 36px Cairo';
+    ctx.font = `bold 36px ${selectedFont}`;
     ctx.fillStyle = '#006699';
     ctx.textAlign = 'center';
     ctx.fillText(name, canvas.width / 2, 500);
