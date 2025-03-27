119125b224fa506fb784c5fff0e95f64d0fd0d36
Ahmed <40410642+abutmim@users.noreply.github.com>
2025-03-27 21:11:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function generateImage(designNumber) {
       2: { fontSize: 38, fontColor: '#0f4c82', x: img.width / 2, y: 957 },
       3: { fontSize: 55, fontColor: '#333333', x: img.width / 2, y: 1660 },
       4: { fontSize: 40, fontColor: '#000000', x: 300, y: 1680 },
-      5: { fontSize: 36, fontColor: '#fefeff', x: img.width / 2, y: 1080 },
+      5: { fontSize: 36, fontColor: '#fefeff', x: img.width / 2, y: 1035 },
       6: { fontSize: 25, fontColor: '#4e615f', x: 1090, y: 880 }
     };
 
