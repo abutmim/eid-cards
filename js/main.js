a3a62b71cf3fca56777afa17eb2ea54051b874c0
Ahmed <40410642+abutmim@users.noreply.github.com>
2025-03-27 04:01:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function generateImage(designNumber) {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
@@ -8,7 +7,7 @@ function generateImage(designNumber) {
   img.src = `images/design${designNumber}.jpg`;
 
   const designSettings = {
-    1: { fontSize: 36, fontColor: '#006699', x: canvas.width * 0.7, y: 1170 },
+    1: { fontSize: 36, fontColor: '#006699', x: canvas.width * 0.7, y: 1250 },
     2: { fontSize: 42, fontColor: '#CC0000', x: canvas.width * 0.7, y: 460 },
     3: { fontSize: 34, fontColor: '#333333', x: canvas.width * 0.3, y: 530 },
     4: { fontSize: 40, fontColor: '#000000', x: 100, y: 480 },
@@ -19,16 +18,16 @@ function generateImage(designNumber) {
   const settings = designSettings[designNumber] || { fontSize: 36, fontColor: '#006699', x: canvas.width / 2, y: 500 };
 
   img.onload = async function () {
-  await document.fonts.ready; // انتظر تحميل الخطوط كلها
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-  ctx.font = `bold ${settings.fontSize}px ${selectedFont}`;
-  ctx.fillStyle = settings.fontColor;
-  ctx.textAlign = 'center';
-  ctx.fillText(name, settings.x, settings.y);
+    await document.fonts.load(`${settings.fontSize}px ${selectedFont}`);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    ctx.font = `bold ${settings.fontSize}px ${selectedFont}`;
+    ctx.fillStyle = settings.fontColor;
+    ctx.textAlign = 'center';
+    ctx.fillText(name, settings.x, settings.y);
 
-  const downloadBtn = document.getElementById('downloadBtn');
-  downloadBtn.href = canvas.toDataURL();
-  downloadBtn.style.display = 'inline-block';
-};
-
+    const downloadBtn = document.getElementById('downloadBtn');
+    downloadBtn.href = canvas.toDataURL();
+    downloadBtn.style.display = 'inline-block';
+  };
+}
